2398a6755f8d682ae7040fe8c3de6df02b27517f
Prod <prodigion@hotmail.com>
2020-08-22 20:31:18 -0400

    Update urls for ABXY hosted wikis

--- rarticle.php
+++ rarticle.php
@@ -1,26 +1,26 @@
 <?php
 $rurl = array(
 				"http://bulbapedia.bulbagarden.net/wiki/Special:Random",
+				"https://armswiki.org/wiki/Special:Random",
 				"https://fireemblemwiki.org/wiki/Special:Random",
 				"http://goldensunwiki.net/Special:Random",
 				"http://starfoxwiki.info/Special:Random",
-				"http://metroidwiki.org/wiki/Special:Random",
 				"http://niwanetwork.org/wiki/index.php/Special:Random",
 				"http://nookipedia.com/wiki/Special:Random",
+				"https://kidicaruswiki.org/wiki/Special:Random",
+				"https://metroidwiki.org/wiki/Special:Random",
 				"https://www.pikminwiki.com/Special:Random",
+				"https://splatoonwiki.org/wiki/Special:Random",
 				"https://www.ssbwiki.com/Special:Random",
 				"http://starfywiki.org/Special:Random",
-				"http://strategywiki.org/wiki/Special:Random",
 				"https://www.mariowiki.com/Special:Random",
-				"http://warswiki.org/wiki/Special:Random",
 				"http://wikibound.info/Special:Random",
 				"http://wikirby.com/wiki/Special:Random",
 				"http://zeldawiki.org/Special:Random",
-				"http://kidicaruswiki.org/Special:Random",
 				"http://dragon-quest.org/wiki/Special:Random",
 				"http://mutecity.org/wiki/Special:Random",
-				"http://splatoonwiki.org/wiki/Special:Random",
-				"https://armswiki.org/wiki/Special:Random",
+				"https://strategywiki.org/wiki/Special:Random",
+				"https://warswiki.org/wiki/Special:Random",
 );
 $random=rand(0,count($rurl));
 header("Location: $rurl[$random]");
