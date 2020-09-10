ce4a140d083ff52c83baa4449c4f328927b6df27
Kevin Payravi <kevinpayravi@gmail.com>
2020-09-10 02:02:11 -0400

    Update CCW link for 2020

--- _menu.php
+++ _menu.php
@@ -1,4 +1,4 @@
 <div id="navigation">
-<a href="../index.php">Home</a> <a href="../about">About</a> <a href="../members">Members</a> <a href="../contact">Contact</a> <a href="http://www.niwanetwork.org/forums/index.php">Forums</a> <a href="../join">Join Us</a> <a href="../cross-wiki-weekend-2019">Cross-Wiki Weekend 2019</a> <a href="/rarticle/">Random Article</a>
+<a href="../index.php">Home</a> <a href="../about">About</a> <a href="../members">Members</a> <a href="../contact">Contact</a> <a href="http://www.niwanetwork.org/forums/index.php">Forums</a> <a href="../join">Join Us</a> <a href="../cross-wiki-weekend-2020">Cross-Wiki Weekend 2020</a> <a href="/rarticle/">Random Article</a>
 </div>
 <div id="social"><a href="http://facebook.com/NIWANetwork"><img src="../images/facebook.png" alt="Facebook" title="Like us on Facebook" /></a> <a href="http://twitter.com/NIWANetwork"><img src="../images/twitter.png" alt="Twitter" title="Follow us on Twitter" /></a></div>
