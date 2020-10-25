1617c63cab0675d93a58f79836e7ac16e7e57a06
Dillion Patrick <dkpat12@users.noreply.github.com>
2020-10-25 18:25:58 -0400

    Begin rewriting css for mobile friendliness
    Implement tons of flexbox to provide flexible styling. Take a mobile-first approach to css.

--- _header.php
+++ _header.php
@@ -1,11 +1,12 @@
 <?php 
 include('_api.php');
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://www.facebook.com/2008/fbml">
+<!DOCTYPE html>
+<html lang="en" xmlns:fb="http://www.facebook.com/2008/fbml">
 <head>
   <meta http-equiv="X-UA-Compatible" content="chrome=1" />
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <title>NIWA - <?php if ($title!='') { echo $title; } else { echo 'Nintendo Independent Wiki Alliance'; } ?></title>
   <link href="../style.css" rel="stylesheet" type="text/css" />
--- _menu.php
+++ _menu.php
@@ -1,14 +1,16 @@
-<div id="navigation">
-    <a href="../">Home</a>
-    <a href="../about">About</a>
-    <a href="../members">Members</a>
-    <a href="../contact">Contact</a>
-    <a href="http://www.niwanetwork.org/forums/index.php">Forums</a>
-    <a href="../join">Join Us</a>
-    <a href="../cross-wiki-weekend-2020">Cross-Wiki Weekend 2020</a>
-    <a href="/rarticle/">Random Article</a>
-</div>
-<div id="social">
-    <a href="https://facebook.com/NIWANetwork"><img src="../images/facebook.png" alt="Facebook" title="Like us on Facebook" /></a>
-    <a href="https://twitter.com/NIWANetwork"><img src="../images/twitter.png" alt="Twitter" title="Follow us on Twitter" /></a>
-</div>
+<nav class="nav" role="navigation">
+    <ul class="primaryNav menu">
+        <li><a href="../">Home</a></li>
+        <li><a href="../about">About</a></li>
+        <li><a href="../members">Members</a></li>
+        <li><a href="../contact">Contact</a></li>
+        <li><a href="http://www.niwanetwork.org/forums/index.php">Forums</a></li>
+        <li><a href="../join">Join Us</a></li>
+        <li><a href="../cross-wiki-weekend-2020">Cross-Wiki Weekend 2020</a></li>
+        <li><a href="/rarticle/">Random Article</a></li>
+    </ul>
+    <ul class="socialNav">
+        <li><a href="https://facebook.com/NIWANetwork"><img src="../images/facebook.png" alt="Facebook" title="Like us on Facebook" /></a></li>
+        <li><a href="https://twitter.com/NIWANetwork" title="Follow us on Twitter"><img src="../images/twitter.png" alt="Twitter" title="Follow us on Twitter" /></a></li>
+    </ul>
+</nav>
