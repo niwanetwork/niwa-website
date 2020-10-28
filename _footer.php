a16e7c2c184fbc108991aa1096ec8422add8a2a1
Dillion Patrick <dkpat12@users.noreply.github.com>
2020-10-27 22:41:39 -0400

    Reasonable mobile navigation implemented
    Flexbox with conversion from horiztonal to verticla on mobile w/ collapsing.
    
    Added flexbox to homepage logos to allow spilling over

--- _footer.php
+++ _footer.php
@@ -1,11 +1,20 @@
 </div>
 
-<div id="footer"><div style="float: left;"><a href="../legal.php">Legal</a></div>
-<a href="http://niwanetwork.org">Nintendo Independent Wiki Alliance</a> &copy; 2010-<?php echo(date('Y')); ?>
+<div id="footer">
+    <div style="float: left;"><a href="../legal.php">Legal</a></div>
+    <a href="http://niwanetwork.org">Nintendo Independent Wiki Alliance</a> &copy; 2010-<?php echo (date('Y')); ?>
 </div>
 
 </div>
 
 <script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
+
+<script>
+    /* Mobile Nav*/
+    document.getElementById("menu-toggle").addEventListener("click", function() {
+        document.getElementById("menuCollapse").classList.toggle("active");
+    });
+</script>
 </body>
+
 </html>
--- _menu.php
+++ _menu.php
@@ -1,16 +1,24 @@
 <nav class="nav" role="navigation">
-    <ul class="primaryNav menu">
-        <li><a href="../">Home</a></li>
-        <li><a href="../about">About</a></li>
-        <li><a href="../members">Members</a></li>
-        <li><a href="../contact">Contact</a></li>
-        <li><a href="http://www.niwanetwork.org/forums/index.php">Forums</a></li>
-        <li><a href="../join">Join Us</a></li>
-        <li><a href="../cross-wiki-weekend-2020">Cross-Wiki Weekend 2020</a></li>
-        <li><a href="/rarticle/">Random Article</a></li>
-    </ul>
-    <ul class="socialNav">
-        <li><a href="https://facebook.com/NIWANetwork"><img src="../images/facebook.png" alt="Facebook" title="Like us on Facebook" /></a></li>
-        <li><a href="https://twitter.com/NIWANetwork" title="Follow us on Twitter"><img src="../images/twitter.png" alt="Twitter" title="Follow us on Twitter" /></a></li>
-    </ul>
+    <div class="mobile-menu">
+        <span>Navigation</span>
+        <div class='menu-icon-container' id="menu-toggle">
+            <span class='menu-icon'></span>
+        </div>
+    </div>
+    <div class="menu" id="menuCollapse">
+        <ul class="primaryNav">
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/members">Members</a></li>
+            <li><a href="/contact">Contact</a></li>
+            <li><a href="http://www.niwanetwork.org/forums/index.php">Forums</a></li>
+            <li><a href="/join">Join Us</a></li>
+            <li><a href="/cross-wiki-weekend-2020">Cross-Wiki Weekend 2020</a></li>
+            <li><a href="/rarticle/">Random Article</a></li>
+        </ul>
+        <ul class="socialNav">
+            <li><a href="https://facebook.com/NIWANetwork"><img src="../edsa-niwa/images/facebook.png" alt="Facebook" title="Like us on Facebook" /></a></li>
+            <li><a href="https://twitter.com/NIWANetwork" title="Follow us on Twitter"><img src="../edsa-niwa/images/twitter.png" alt="Twitter" title="Follow us on Twitter" /></a></li>
+        </ul>
+    </div>
 </nav>
