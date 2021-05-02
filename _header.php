264d50acef69ea7f20f599712b8180de619c7004
Elaeagnifolia <j.wong205914@gmail.com>
2021-05-01 19:30:43 -0500

    Fixing accidentally removed slash.

--- _header.php
+++ _header.php
@@ -10,7 +10,7 @@ $dataHelper = new NiwaDataHelper();
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <title>NIWA - <?php if ($title!='') { echo $title; } else { echo 'Nintendo Independent Wiki Alliance'; } ?></title>
-  <link href="style.css" rel="stylesheet" type="text/css" />
+  <link href="/style.css" rel="stylesheet" type="text/css" />
 
   <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
