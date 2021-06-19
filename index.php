d700c421807ca97e0f4770b73ae605c129c5c8d3
Kevin Payravi <kevinpayravi@gmail.com>
2021-06-19 05:34:27 -0500

    Starting revamped homepage

--- index.php
+++ index.php
@@ -3,16 +3,21 @@ include('_header.php');
 ?>
 
 <div class="main">
-   <div id="logos">
-      <?php
-      foreach ($dataHelper->getMemberWikis('en') as $wiki) {
-         echo "
-            <a href='{$dataHelper->getWikiMainpage($wiki)}'>
-               <img src='{$wiki->logo}' alt='{$wiki->title}' />
-            </a>
-         ";
-      }
-      ?>
+   The Nintendo Independent Wiki Alliance.
+
+   <div class="flex-container">
+      <div class="flex-section">
+         <h2 style="margin: 15px 0px 5px 0px;">30+ members</h2>
+         covering Nintendo subjects across 3 languages
+      </div>
+      <div class="flex-section">
+         <h2 style="margin: 15px 0px 5px 0px;">200,000+ articles</h2>
+         across all our member wikis
+      </div>
+      <div class="flex-section">
+         <h2 style="margin: 15px 0px 5px 0px;">10+ affiliates</h2>
+         supporting and collaborating in our mission
+      </div>
    </div>
 </div>
 
