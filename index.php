d3a0bb29fd57909677d6857617eb946c7c514be4
Kevin Payravi <kevinpayravi@gmail.com>
2021-06-19 20:08:46 -0500

    Expanding homepage

--- index.php
+++ index.php
@@ -3,22 +3,64 @@ include('_header.php');
 ?>
 
 <div class="main">
-   The Nintendo Independent Wiki Alliance.
+   <h1>Welcome to the Nintendo Independent Wiki Alliance</h1>
+   <p>
+      We are a network of independent wikis working together since 2010 to bring fans quality coverage of all things Nintendo. Together with our affiliates, we collaborate and support one another in our mission. 
+   </p>
+
+   <p>
+      We are driven by our focus on our fans:
+      <ul>
+         <li><b>Fan communities should be run by the fans</b>, and not corporate entities that don't put the community first.</li>
+         <li><b>Fan communities should be run for the fans</b>, by people who will always think about how they can continue to enhance the enjoyment of everyone in the community.</li>
+         <li><b>Fan communities should be run together with the fans</b>, embracing a spirit of co-operation and camaraderie.</li>
+      </ul>
+   </p>
 
    <div class="flex-container">
       <div class="flex-section">
          <h2 style="margin: 15px 0px 5px 0px;">30+ members</h2>
-         covering Nintendo subjects across 3 languages
+         covering Nintendo subjects
+         <br/><br/>
       </div>
       <div class="flex-section">
          <h2 style="margin: 15px 0px 5px 0px;">200,000+ articles</h2>
-         across all our member wikis
+         across three languages
+         <br/><br/>
       </div>
       <div class="flex-section">
          <h2 style="margin: 15px 0px 5px 0px;">10+ affiliates</h2>
-         supporting and collaborating in our mission
+         supporting our mission
+         <br/><br/>
       </div>
    </div>
+
+   <p>
+      Learn more about us on our <a href="/about">about page</a>, and check out our <a href="/members">members</a> and <a href="/affiliates">affiliates</a>.
+   </p>
+
+   <h1>Stay engaged</h1>
+
+   <p>
+      We have a public Discord servers for Nintendo and wiki fans alike, as well as social media profiles to keep up to date with going-ons between our members.
+   </p>
+
+   <div class="flex-container">
+      <div class="flex-section" style="text-align: center; background-color: #5865F2;">
+         <a style="color: white;" href="https://discord.gg/59Mq6qB">Join our Discord</a>
+      </div>
+      <div class="flex-section" style="text-align: center; background-color: #1DA1F2;">
+         <a style="color: white;" href="https://twitter.com/niwanetwork">Follow on Twitter</a>
+      </div>
+      <div class="flex-section" style="text-align: center; background-color: #4267B2;">
+         <a style="color: white;" href="https://www.facebook.com/NIWANetwork">Like on Facebook</a>
+      </div>
+   </div>
+
+   <p>
+      You can also engage with our <a href="/members">members</a> and <a href="/affiliates">affiliates</a> through their own Discord servers and social media.
+   </p>
+
 </div>
 
 <?php
