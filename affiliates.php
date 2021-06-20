c336368e4478754269308a7cefdb2c04b23a62e9
Kevin Payravi <kevinpayravi@gmail.com>
2021-06-19 20:27:14 -0500

    Adding affiliates description

--- affiliates.php
+++ affiliates.php
@@ -7,6 +7,10 @@ include('_header.php');
 <div class="main">
     <h1>NIWA Affiliates</h1>
 
+    <p>
+        Our affiliates consist of great websites, communities, and organizations that share our goals and collaborative spirit, and typically cover videogames or other media. Affiliates have access to our members-only Discord community where we support each other's projects. If you're interested in becoming an affiliate and think you'd be a great fit, please <a href="/contact">reach out to us</a>!
+    </p>
+
     <section>
         <?php
         foreach ($dataHelper->getAffiliates() as $affiliate) {
