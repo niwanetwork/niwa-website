a44534c184af137f7e6cd7209ac0b1f442f35d50
Kevin Payravi <kevinpayravi@gmail.com>
2021-06-19 19:39:26 -0500

    Adding Discord widget to sidebar

--- _sidebar.php
+++ _sidebar.php
@@ -20,7 +20,10 @@
       </script>
     </div>
   </div>
-
+  <div class="box" id="box-discord">
+    <div class="title">Discord</div>
+      <iframe src="https://discord.com/widget?id=701089448125005835&theme=dark" width="100%" height="300" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+  </div>
   <div class="box" id="box-affiliates">
     <div class="title">Affiliates</div>
     <div class="post">
