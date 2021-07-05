a1725b150b819b1db95d054669a52dfea094950e
Kevin Payravi <kevinpayravi@gmail.com>
2021-07-05 14:44:33 -0500

    Use labels for contact form, refactor styling

--- contact.php
+++ contact.php
@@ -10,21 +10,20 @@ include('_header.php');
 		You may use this form to contact the current Coordinator of NIWA for discussion about the organization, including membership and affiliation applications.
 		If you would like to reach out to a specific member wiki, please visit that wiki's website to find their contact information. You may also join our <a href="https://discord.gg/59Mq6qB">Discord server</a> to chat with members of the community.
 	</p>
-	<form method="post" action="/mail.php">
-		<p class="form">Name:</p>
-		<input type="text" name="name" class="contact" />
-		<p class="form">E-mail Address:</p>
-		<input type="text" name="email" class="contact" />
-		<p class="form">Subject:</p>
-		<input type="text" name="subject" class="contact" />
-		<p class="form">Message:</p>
-		<textarea rows="20" name="message" class="message"></textarea>
-		<p class="form">What does the "N" in NIWA stand for?:</p>
-		<input type="text" name="captcha" class="contact" />
-		<br/><br/>
+	<form class="contact" method="post" action="/mail.php">
+		<label for="name">Name</label>
+		<input id="name" type="text" name="name" />
+		<label for="email">E-mail Address</label>
+		<input id="email" type="text" name="email" />
+		<label for="subject">Subject</label>
+		<input id="subject" type="text" name="subject" />
+		<label for="message">Message</label>
+		<textarea id="message" rows="20" name="message"></textarea>
+		<label for="captcha">What does the "N" in NIWA stand for?</label>
+		<input id="captcha" type="text" name="captcha" />
+		<br/>
 		<input type="submit" value="Submit" name="submit" class="submit" />
 	</form>
-
 </div>
 
 <?php
