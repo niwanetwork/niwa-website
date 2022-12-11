f26a9b6bd7a3bf0fdb49e6803528032809466883
Kevin Payravi <kevinpayravi@gmail.com>
2022-12-11 16:50:19 -0600

    Rm named arguments; server's PHP version doesn't support

--- cross-wiki-weekend-2021.php
+++ cross-wiki-weekend-2021.php
@@ -87,7 +87,7 @@ THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
         <h3 class="text-center">English Wikis</h3>
         <div class="text-center cww-grid">
             <?php
-            $wikis = $dataHelper->getMemberWikis(languageCode: 'en', includeFormer: TRUE);
+            $wikis = $dataHelper->getMemberWikis('en', TRUE);
             foreach ($dataHelper->getCWW('2021', 'en') as $cww) {
                 $id = $cww->id;
                 $wiki = $wikis[array_search($id, array_column($wikis, 'id'))];
@@ -107,7 +107,7 @@ THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
         <h3 class="text-center">Italian Wikis</h3>
         <div class="text-center cww-grid">
             <?php
-            $wikis = $dataHelper->getMemberWikis(languageCode: 'it', includeFormer: TRUE);
+            $wikis = $dataHelper->getMemberWikis('it', TRUE);
             foreach ($dataHelper->getCWW('2021', 'it') as $cww) {
                 $id = $cww->id;
                 $wiki = $wikis[array_search($id, array_column($wikis, 'id'))];
@@ -236,7 +236,7 @@ THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
         <h3 class="text-center">Wiki in italiano</h3>
         <div class="text-center cww-grid">
             <?php
-            $wikis = $dataHelper->getMemberWikis(languageCode: 'it', includeFormer: TRUE);
+            $wikis = $dataHelper->getMemberWikis('it', TRUE);
             foreach ($dataHelper->getCWW('2021', 'it') as $cww) {
                 $id = $cww->id;
                 $wiki = $wikis[array_search($id, array_column($wikis, 'id'))];
@@ -256,7 +256,7 @@ THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
         <h3 class="text-center">Wiki in inglese</h3>
         <div class="text-center cww-grid">
             <?php
-            $wikis = $dataHelper->getMemberWikis(languageCode: 'en', includeFormer: TRUE);
+            $wikis = $dataHelper->getMemberWikis('en', TRUE);
             foreach ($dataHelper->getCWW('2021', 'en') as $cww) {
                 $id = $cww->id;
                 $wiki = $wikis[array_search($id, array_column($wikis, 'id'))];
