40112f0f515b202afac155df055d9a6711a0c345
Kevin Payravi <kevinpayravi@gmail.com>
2023-07-16 22:32:10 -0500

    Removing Twitter widget, thanks Elon
    Hope you go bankrupt ❤️

--- _sidebar.php
+++ _sidebar.php
@@ -1,25 +1,4 @@
 <div class="sidebar">
-  <div class="box" id="box-twitter">
-    <div class="title">Latest Tweets</div>
-    <div class="post">
-      <a class="twitter-timeline" data-dnt="true" href="https://twitter.com/NIWANetwork"
-        data-widget-id="390347444321394689" height="350px">
-        Tweets by @NIWANetwork
-      </a>
-      <script>
-        ! function(d, s, id) {
-          var js, fjs = d.getElementsByTagName(s)[0],
-            p = /^http:/.test(d.location) ? 'http' : 'https';
-          if (!d.getElementById(id)) {
-            js = d.createElement(s);
-            js.id = id;
-            js.src = p + "://platform.twitter.com/widgets.js";
-            fjs.parentNode.insertBefore(js, fjs);
-          }
-        }(document, "script", "twitter-wjs");
-      </script>
-    </div>
-  </div>
   <div class="box" id="box-discord">
     <div class="title">Discord</div>
     <div class="post">
