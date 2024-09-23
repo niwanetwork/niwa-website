d9df220412e857d15a0da3fbc34437ab350478f7
PanchamBro <panchambro@proton.me>
2024-09-23 02:19:55 -0400

    Adding Bluesky links (#145)

--- _api.php
+++ _api.php
@@ -228,6 +228,9 @@ class NiwaDataHelper
 		if (isset($member->mastodon)) {
 			$links .= $this->generateMemberLink($member->mastodon, '<img src="/images/mastodon.png" alt="Mastodon" width="20" />');
 		};
+		if (isset($member->bluesky)) {
+			$links .= $this->generateMemberLink($member->bluesky, '<img src="/images/bluesky.png" alt="Bluesky" width="20" />');
+		};
 		if (isset($member->twitch)) {
 			$links .= $this->generateMemberLink($member->twitch, '<img src="/images/twitch.png" alt="Twitch" width="20" />');
 		};
